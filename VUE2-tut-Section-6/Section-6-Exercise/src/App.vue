b0728158d3cf7c2ac6c29d7f9846c338984bdd36
16495836 <1lee.burrell@gmail.com>
2018-10-12 10:40:12 -0400

    completed-section-6-exercise

--- VUE2-tut-Section-6/Section-6-Exercise/src/App.vue
+++ VUE2-tut-Section-6/Section-6-Exercise/src/App.vue
@@ -1,38 +1,25 @@
 <template>
     <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <header>
-                    <h1>Server Status</h1>
-                </header>
-            </div>
-        </div>
+<ex-header></ex-header>
         <hr>
-        <div class="row">
-            <div class="col-xs-12 col-sm-6">
-                <ul class="list-group">
-                    <li class="list-group-item"
-                            v-for="index in 5">
-                        Server #{{ index }}
-                    </li>
-                </ul>
-            </div>
-            <div class="col-xs-12 col-sm-6">
-                <p>Server Details are currently not updated</p>
-            </div>
-        </div>
+<ex-dashboard></ex-dashboard>
         <hr>
-        <div class="row">
-            <div class="col-xs-12">
-                <footer>
-                    <p>All Servers are managed here</p>
-                </footer>
-            </div>
-        </div>
+<ex-footer></ex-footer>
     </div>
 </template>
 
 <script>
+import Header from "./Header.vue";
+import Dashboard from "./Dashboard.vue";
+import Footer from "./Footer.vue";
+
+export default {
+  components: {
+    "ex-header": Header,
+    "ex-dashboard": Dashboard,
+    "ex-footer": Footer
+  }
+};
 </script>
 
 <style>

8a626cf67f88ffe8c1e4a60fcbfebf7cc426f062
16495836 <1lee.burrell@gmail.com>
2018-10-15 10:27:26 -0400

    Exercise-Section-8-Start

--- VUE2-tut-Section-8/Section-8-Exercise/src/components/Server/ServerDetails.vue
+++ VUE2-tut-Section-8/Section-8-Exercise/src/components/Server/ServerDetails.vue
@@ -6,8 +6,20 @@
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      activeServers: [
+        { id: 1001, status: "Normal" },
+        { id: 1002, status: "Elevated" },
+        { id: 1003, status: "Attention" },
+        { id: 1004, status: "Critical" },
+        { id: 1005, status: "Normal" }
+      ]
+    };
+  }
+};
 </script>
 
 <style>
-
 </style>
--- VUE2-tut-Section-8/Section-8-Exercise/src/main.js
+++ VUE2-tut-Section-8/Section-8-Exercise/src/main.js
@@ -1,7 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+
+export const eventBus = new Vue({
+  methods: {
+    resetStatus(status) {
+      this.$emit("changedStatus", status);
+    }
+  }
+});
 
 new Vue({
-  el: '#app',
+  el: "#app",
   render: h => h(App)
-})
+});

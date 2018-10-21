6de9ecb6fffec01915091f778eba400ec5d3ead8
16495836 <1lee.burrell@gmail.com>
2018-10-21 09:35:39 -0400

    Section-9-Lessons

--- VUE2-tut-Section-9/lesson-118/src/App.vue
+++ VUE2-tut-Section-9/lesson-118/src/App.vue
@@ -2,10 +2,18 @@
 <div class="container">
   <div class="row">
     <div class="col-xs-12">
-      <app-quote quote="a wonderful quote">
+      <button @click="selectedComponent = 'appQuote'">Quote</button>
+      <button @click="selectedComponent = 'appAuthor'">Author</button>
+      <button @click="selectedComponent = 'appNew'">New</button>
+      <hr>
+      <p>{{ selectedComponent }}</p>
+      <keep-alive>
+      <component :is="selectedComponent"><p>Default Content</p></component>
+      </keep-alive>
+ <!--     <app-quote quote="a wonderful quote">
 <h2 slot="title">A Quote Happens...</h2>
 <p slot="content">a wonderful quote</p>
-      </app-quote>
+      </app-quote> -->
     </div>
   </div>
 </div>
@@ -13,10 +21,21 @@
 </template>
 
 <script>
+import Author from "./components/Author.vue";
+import New from "./components/New.vue";
 import Quote from "./components/Quote.vue";
+
 export default {
+  data() {
+    return {
+      quoteTitle: "The Quote",
+      selectedComponent: "app-quote"
+    };
+  },
   components: {
-    "app-quote": Quote
+    appQuote: Quote,
+    appAuthor: Author,
+    appNew: New
   }
 };
 </script>
---
+++ VUE2-tut-Section-9/lesson-118/src/components/New.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+    <h3>New Quote</h3>
+    <p>Press the increase button an come back to see if this page re-created</p>
+    <button @click="counter++">Increase</button>
+    <h4>{{ counter }}</h4>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0
+    };
+  },
+  activated() {
+    console.log("Activated!");
+  },
+  deactivated() {
+    console.log("Deactivated!");
+  },
+  destroyed() {
+    console.log("Destroyed!");
+  }
+};
+</script>
+
+<style>
+</style>

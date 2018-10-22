182577bea1027ba4dab6dc88fd408553f12befda
16495836 <1lee.burrell@gmail.com>
2018-10-21 20:16:40 -0400

    ...

---
+++ VUE2-tut-Section-10/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+    
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      maxQuotes: 10,
+      quotes: ["a default placeholder quote"]
+    };
+  }
+};
+</script>
+
+<style>
+</style>

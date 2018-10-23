4b3ebffc9cad6bda9f5a21d40c5136c9913e3cda
16495836 <1lee.burrell@gmail.com>
2018-10-23 10:08:11 -0400

    Quote-Lesson-Complete

--- VUE2-tut-Section-10/src/App.vue
+++ VUE2-tut-Section-10/src/App.vue
@@ -1,18 +1,28 @@
 <template>
     <div class="container">
+      <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
         <app-new-quote @quoteAdded="newQuote"></app-new-quote>
-    <app-quotes v-for="quote in quotes">{{ quote }}</app-quotes>
+    <app-quotes v-for="(quote, index) in quotes" @click.native="deleteQuote(index)">{{ quote }}</app-quotes>
+    <div class="row">
+        <div class="col-sm-12 text-center">
+            <div class="alert alert-info">
+                Info: Click on a quote to delete it
+            </div>
+        </div>
+    </div>
     </div>
 </template>
 
 <script>
 import NewQuote from "./components/NewQuote.vue";
 import Quotes from "./components/Quotes.vue";
+import Header from "./components/Header.vue";
 
 export default {
   components: {
     appQuotes: Quotes,
-    appNewQuote: NewQuote
+    appNewQuote: NewQuote,
+    appHeader: Header
   },
   data() {
     return {
@@ -23,6 +33,12 @@ export default {
   methods: {
     newQuote(quote) {
       this.quotes.push(quote);
+      if (this.quotes.length >= this.maxQuotes) {
+        alert("Max Quotes Exceeded! Please delete a quote to continue.");
+      }
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
     }
   }
 };

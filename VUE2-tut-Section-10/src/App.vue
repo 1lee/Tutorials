377898f891aeb97ca2fc6856aeaf9f0de51be469
16495836 <1lee.burrell@gmail.com>
2018-10-22 21:54:35 -0400

    Quote-app-assignment

--- VUE2-tut-Section-10/src/App.vue
+++ VUE2-tut-Section-10/src/App.vue
@@ -1,16 +1,29 @@
 <template>
     <div class="container">
-    
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+    <app-quotes v-for="quote in quotes">{{ quote }}</app-quotes>
     </div>
 </template>
 
 <script>
+import NewQuote from "./components/NewQuote.vue";
+import Quotes from "./components/Quotes.vue";
+
 export default {
+  components: {
+    appQuotes: Quotes,
+    appNewQuote: NewQuote
+  },
   data() {
     return {
       maxQuotes: 10,
-      quotes: ["a default placeholder quote"]
+      quotes: ["butterfly", "mickey mouse", "a default placeholder quote"]
     };
+  },
+  methods: {
+    newQuote(quote) {
+      this.quotes.push(quote);
+    }
   }
 };
 </script>

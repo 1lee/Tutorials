553c56ef3c55e472430fccda3831ff00b3dae5f0
16495836 <1lee.burrell@gmail.com>
2018-10-28 17:12:48 -0400

    ...

--- VUE2-tut-Section-12/lesson-158/src/App.vue
+++ VUE2-tut-Section-12/lesson-158/src/App.vue
@@ -1,5 +1,13 @@
 <template>
-  
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+<h1>Built In Directives</h1>
+<p></p>
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <script>

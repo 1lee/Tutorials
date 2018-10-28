4cc262bf8d27734cd0100b37a83e0a01df34ee1b
16495836 <1lee.burrell@gmail.com>
2018-10-28 10:53:30 -0400

    Completed

---
+++ VUE2-tut-Section-11/Exercise/src/FullNameControl.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="form-group">
+      <label for="firstname">First Name</label>
+      <input type="text" id="firstname" class="form-control" v-model="firstName" @keyup="emitFullName">
+    </div>
+    <hr>
+    <div class="form-group">
+      <label for="lastname">Last Name</label>
+      <input type="text" id="lastname" class="form-control" v-model='lastName' @keyup="emitFullName">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      firstName: "",
+      lastName: ""
+    };
+  },
+  methods: {
+    emitFullName() {
+      let fullName = this.firstName + " " + this.lastName;
+      this.$emit("onNameChanged", fullName);
+    }
+  }
+};
+</script>
+
+<style>
+</style>

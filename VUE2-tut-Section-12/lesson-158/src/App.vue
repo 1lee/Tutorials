ec7dd3ffab9a9e9244b5efe12477f0e03c9893f0
16495836 <1lee.burrell@gmail.com>
2018-10-29 10:17:40 -0400

    ...

--- VUE2-tut-Section-12/lesson-158/src/App.vue
+++ VUE2-tut-Section-12/lesson-158/src/App.vue
@@ -11,15 +11,57 @@
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Custom Directives</h1>
-        <p v-highlight>Custom directive example (green highlight)</p>
+        <p v-highlight:background.delayed="'yellow'">Custom directive example(global)</p>
  
+         <p v-local-highlight:background.delayed.blink="'yellow'">Custom directive example(local) </p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  directives: {
+    localHighlight: {
+      bind(el, binding, vnode) {
+        let delay = 0;
+
+        if (binding.modifiers["delayed"]) {
+          delay = 2000;
+        }
+        if (binding.modifiers["blink"]) {
+          let mainColor = binding.value;
+          let secondColor = "orange";
+          let currentColor = mainColor;
+
+          setTimeout(() => {
+            setInterval(() => {
+              currentColor == mainColor
+                ? (currentColor = secondColor)
+                : (currentColor = mainColor);
+
+              if (binding.arg == "background") {
+                el.style.backgroundColor = currentColor;
+              } else {
+                el.style.color = currentColor;
+              }
+            }, 1000);
+            //adjusting styling according to passed arguments after directive
+          }, delay);
+        } else {
+          setTimeout(() => {
+            //adjusting styling according to passed arguments after directive
+            if (binding.arg == "background") {
+              el.style.backgroundColor = binding.value;
+            } else {
+              el.style.color = binding.value;
+            }
+          }, delay);
+        }
+      }
+    }
+  }
+};
 </script>
 
 <style>
--- VUE2-tut-Section-12/lesson-158/src/main.js
+++ VUE2-tut-Section-12/lesson-158/src/main.js
@@ -4,8 +4,27 @@ import App from "./App.vue";
 Vue.config.productionTip = false;
 
 Vue.directive("highlight", {
-  bind(el) {
-    el.style.backgroundColor = "lightgreen";
+  bind(el, binding) {
+    // hard-coded color
+    // el.style.backgroundColor = "lightgreen";
+
+    //user directed color
+    //el.style.backgroundColor = binding.value;
+
+    let delay = 0;
+
+    if (binding.modifiers["delayed"]) {
+      delay = 2000;
+    }
+
+    setTimeout(() => {
+      //adjusting styling according to passed arguments after directive
+      if (binding.arg == "background") {
+        el.style.backgroundColor = binding.value;
+      } else {
+        el.style.color = binding.value;
+      }
+    }, delay);
   }
 });
 

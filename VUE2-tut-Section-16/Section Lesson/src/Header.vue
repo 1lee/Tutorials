d536c269f31d6ed15440de4e8a557c83851cca95
16495836 <1lee.burrell@gmail.com>
2018-11-09 15:27:53 -0500

    Lecture 235

---
+++ VUE2-tut-Section-16/Section Lesson/src/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <ul class="nav nav-pills">
+
+      <router-link class="nav-item" to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
+  
+<router-link class="nav-item" to="/user/1" tag="li" active-class="active"><a>User One</a></router-link>
+  
+  <router-link class="nav-item" to="/user/2" tag="li" active-class="active"><a>User Two</a></router-link>
+
+</ul>
+</template>
--- VUE2-tut-Section-16/Section Lesson/src/components/user/User.vue
+++ VUE2-tut-Section-16/Section Lesson/src/components/user/User.vue
@@ -1,4 +1,28 @@
 <template>
+<div>
     <h1>The User Page</h1>
-
+    <p>User ID: {{ id }}</p>
+<hr>
+<button class="btn btn-primary" @click="navigateToHome">Go Home</button>
+</div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: this.$route.params.id
+    };
+  },
+  watch: {
+    $route(to, from) {
+      this.id = to.params.id;
+    }
+  },
+  methods: {
+    navigateToHome() {
+      this.$router.push("/");
+    }
+  }
+};
+</script>

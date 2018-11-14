ff1af2f06c110e2df8353e859e17825938a75799
16495836 <1lee.burrell@gmail.com>
2018-11-14 08:46:21 -0500

    Folder-Structure

--- VUE2-tut-Section-18/Final Project/src/components/portfolio/Portfolio.vue
+++ VUE2-tut-Section-18/Final Project/src/components/portfolio/Portfolio.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1>Header Component</h1>
+    <h1>Portfolio Component</h1>
 </template>
 
 <script>
---
+++ VUE2-tut-Section-18/Final Project/src/components/portfolio/Stock.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Stock Component (Portfolio)</h1></template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

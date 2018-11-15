87da233211d56ff7ec44ddd73226e0fb4dbc5c84
16495836 <1lee.burrell@gmail.com>
2018-11-15 10:46:04 -0500

    header

--- VUE2-tut-Section-18/Final Project/src/components/Header.vue
+++ VUE2-tut-Section-18/Final Project/src/components/Header.vue
@@ -1,5 +1,40 @@
 <template>
-    <h1>Header Component</h1>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <router-link class="navbar-brand" to="/">Stock Trader</router-link>
+
+  <ul class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="nav navbar-nav">
+
+      <router-link class="nav-item active" tag="li" to="/portfolio" activeClass="active">
+        <a>Portfolio</a>
+      </router-link>
+
+      <router-link class="nav-item active" tag="li" to="/stocks" activeClass="active">
+        <a>Stocks</a>
+      </router-link>
+
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">End Day</a></li>
+        <li class="dropdown">
+        <a class="dropdown-toggle" 
+        href="#" id="navbarDropdown" 
+        role="button" 
+        data-toggle="dropdown" 
+        aria-haspopup="true" 
+        aria-expanded="false">
+          Save & Load
+          <span class="caret"></span>
+        </a>
+        <ul class="dropdown-menu">
+          <li class="dropdown-item"><a href="#">Save Data</a></li>
+          <li class="dropdown-item"><a href="#">Load Data</a></li>
+
+        </ul>
+      </li>
+      </ul>
+  </ul>
+</nav>
 </template>
 
 <script>

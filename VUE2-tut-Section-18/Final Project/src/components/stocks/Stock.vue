9771fc7b40d5532695b205f484c4ea2377bfb8b8
16495836 <1lee.burrell@gmail.com>
2018-11-18 19:55:17 -0500

    ...

--- VUE2-tut-Section-18/Final Project/src/components/stocks/Stock.vue
+++ VUE2-tut-Section-18/Final Project/src/components/stocks/Stock.vue
@@ -1,10 +1,60 @@
 <template>
-    <h1>Stock Component (Stock)</h1>
+<div class="col-md-4 col-sm-6">
+<div class="panel panel-succes">
+<div class="panel-heading">
+    <h3 class="panel-title">
+        {{ stock.name }}
+        <small>
+            (price ${{ stock.price }})
+        </small>
+    </h3>
+</div>
+<div class="panel-body">
+    <div class="pull-left">
+        <input
+        type="number"
+        class="form-control"
+        placeholder="Quantity"
+        v-model="quantity"/>
+        
+    </div>
+    <div class="pull-right">
+        <button
+        class="btn btn-success"
+        @click="buyStock"
+        :disabled="quantity <= 0 || !isInt(quantity)"
+
+        >BUY
+        </button>
+    </div>
+</div>
+</div>
+</div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      quantity: 0
+    };
+  },
+  props: ["stock"],
+  methods: {
+    buyStock() {
+      const order = {
+        id: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity
+      };
+      console.log(order);
+      this.quantity = 0;
+    },
+    isInt(n) {
+      return n % 1 === 0;
+    }
+  }
+};
 </script>
-
 <style>
 </style>
---
+++ VUE2-tut-Section-18/Final Project/src/components/stocks/Stocks.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+     <app-stock v-for="stock in stocks" :key="stock.id" :stock="stock">{{ stock.name }}</app-stock>
+
+    </div>
+</template>
+
+<script>
+import Stock from "./Stock.vue";
+
+export default {
+  components: {
+    appStock: Stock
+  },
+  data() {
+    return {
+      stocks: [
+        { id: 1, name: "KRUSTY CRAB", price: 12 },
+        { id: 2, name: "MOES", price: 75 },
+        { id: 3, name: "BUBBAGUMP", price: 175 },
+        { id: 4, name: "BLAMMO", price: 17 }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>

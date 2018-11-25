15fa437f127d29c3010c0129d5e865b757efe3a0
16495836 <1lee.burrell@gmail.com>
2018-11-25 16:52:06 -0500

    randomize-stock-price

--- VUE2-tut-Section-18/Final Project/src/components/Header.vue
+++ VUE2-tut-Section-18/Final Project/src/components/Header.vue
@@ -14,9 +14,9 @@
       </router-link>
 
     </ul>
-    <strong class="navbar-text navbar-right">Funds: {{ funds }}</strong>
+    <strong class="navbar-text navbar-right">Funds: {{ funds | currency}}</strong>
     <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">End Day</a></li>
+        <li><a href="#" @click="endDay">End Day</a></li>
         <li class="dropdown">
         <a class="dropdown-toggle" 
         href="#" id="navbarDropdown" 
@@ -39,10 +39,18 @@
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
   computed: {
     funds() {
       return this.$store.getters.funds;
+    }
+  },
+  methods: {
+    ...mapActions(["randomizeStocks"]),
+    endDay() {
+      this.randomizeStocks();
     }
   }
 };

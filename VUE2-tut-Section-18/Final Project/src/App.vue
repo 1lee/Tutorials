24a50fbf98933a67c16851421ad4a940583bb1de
16495836 <1lee.burrell@gmail.com>
2018-11-25 21:25:07 -0500

    ...

--- VUE2-tut-Section-18/Final Project/src/App.vue
+++ VUE2-tut-Section-18/Final Project/src/App.vue
@@ -1,13 +1,14 @@
 <template>
-    <div class="container">
-        <app-header></app-header>
-        <div class="row">
-<div class="col-md-12">
-<router-view></router-view>
-
-</div>
-        </div>
+  <div class="container">
+    <app-header></app-header>
+    <div class="row">
+      <div class="col-xs-12">
+        <transition name="slide" mode="out-in"> 
+          <router-view></router-view> 
+          </transition>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -17,11 +18,41 @@ export default {
   components: {
     appHeader: Header
   },
-  created(){
-    this.$store.dispatch('initStocks');
+  created() {
+    this.$store.dispatch("initStocks");
   }
 };
 </script>
 
 <style>
+body {
+  padding: 30px;
+}
+.slide-enter-active {
+  animation: slide-out 200ms ease-out forwards;
+}
+.slide-leave-active {
+  /* animation: slide-out 200ms ease-out forwards; */
+}
+@keyframes slide-in {
+  to {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+  from {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+
+@keyframes slide-out {
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
+  from {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+}
 </style>
